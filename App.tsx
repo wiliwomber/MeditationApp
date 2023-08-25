3433175b8215562d9c523add2c94218172c5356a
wiliwomber <johannes.waldorf@outlook.de>
2023-08-26 01:24:08 +0200

    add gong

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Button, Layout, Text } from '@ui-kitten/components';
 import theme from './theme.json'
+import { Audio } from 'expo-av';
 
 const HomeScreen = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category='h1'>HOME</Text>
+    <Text category='h1'>Meditation</Text>
+    <Button onPress={playSound}>Play gong</Button>
   </Layout>
 );
+
+async function playSound() {
+  console.log('Loading Sound');
+  const { sound } = await Audio.Sound.createAsync(require('./assets/gong.mp3')
+  );
+  await sound.playAsync();
+}
+
 
 export default () => (
   <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
     <HomeScreen />
   </ApplicationProvider>
-);  
+);
+
